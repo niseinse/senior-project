2d5a19e278f12378695eecdfdf1c5c035fbed277
niseinse <kisein@naver.com>
2016-10-06 17:22:49 +0900

    Change constant number with #define

--- result_extractor/result_extractor.c
+++ result_extractor/result_extractor.c
@@ -3,23 +3,24 @@
 #include <string.h>
 #include <float.h>
 
-#define MAX 1000
+#define MAX_ARR_SIZE 1000
+#define RESULT_COUNT 5
 
 int main(int argc, char* argv[])
 {
 	// variable definition
-	char result_tmp_arr[5][MAX];
+	char result_tmp_arr[RESULT_COUNT][MAX_ARR_SIZE];
 	char* string_trim = NULL;
-	double result_accuracy_arr[5];
-	char* result_building_arr[5];
+	double result_accuracy_arr[RESULT_COUNT];
+	char* result_building_arr[RESULT_COUNT];
 	int i, j;
 	
 	// extract raw data
-	fgets(result_tmp_arr[0], MAX-1, stdin);
-	fgets(result_tmp_arr[0], MAX-1, stdin);
-	for(i=0; i<5; i++)
+	fgets(result_tmp_arr[0], MAX_ARR_SIZE-1, stdin);
+	fgets(result_tmp_arr[0], MAX_ARR_SIZE-1, stdin);
+	for(i=0; i<RESULT_COUNT; i++)
 	{
-		fgets(result_tmp_arr[i], MAX-1, stdin);
+		fgets(result_tmp_arr[i], MAX_ARR_SIZE-1, stdin);
 		result_tmp_arr[i][8] = '\0';
 		sscanf(result_tmp_arr[i], "%lf", &result_accuracy_arr[i]);
 		
@@ -33,9 +34,9 @@ int main(int argc, char* argv[])
 	}
 	
 	// integrate same building data
-	for(i=0; i<5; i++)
+	for(i=0; i<RESULT_COUNT; i++)
 	{
-		for(j=4; j>i; j--)
+		for(j=RESULT_COUNT-1; j>i; j--)
 		{
 			if(strcmp(result_building_arr[i], result_building_arr[j]) == 0)
 			{
@@ -46,7 +47,7 @@ int main(int argc, char* argv[])
 	}
 	
 	// print effective data
-	for(i=0; i<5; i++)
+	for(i=0; i<RESULT_COUNT; i++)
 	{
 		if(result_accuracy_arr[i] > DBL_EPSILON)
 		{
